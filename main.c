d2d09ee5cf0d61fd737ad61f5db06e84eacb7d71
korokorotune <ev40008@meiji.ac.jp>
2015-12-16 09:56:51 +0900

    初期関数簡略化

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ typedef struct{
     int date[DAY][PERIOD];
 }COMPULSORY;
 
-int MidCheck(int newDay,int newPeriod,int newRoom,SUBJECT targetSubject,ROOM room[],TEACHER teacher[],CLASS class[]);
+int MidCheck(int newDay,int newPeriod,int newRoom,SUBJECT targetSubject,ROOM room[],TEACHER teacher[],CLASS class[], COMPULSORY compulsoryData);
 int FinalCheck(ROOM room[],TEACHER teacher[],CLASS class[],SUBJECT subject[],COMPULSORY compulsoryData);
 
 int main(){
@@ -44,23 +44,23 @@ int main(){
     int count=0;                //countはwhileループ回した回数
     SUBJECT subject[NUMOFSUBJ]={
         {0,0,0,0,0,0,1},
-        {1,2,0,0,0,0,1},
-        {2,1,0,0,0,0,1},
+        {1,2,3,0,0,0,1},
+        {2,1,4,0,0,0,1},
         {3,2,1,0,0,0,1},
-        {1,3,1,0,0,0,1},
+        {1,3,3,0,0,0,1},
         {2,4,1,0,0,0,1},
         {4,1,2,0,0,0,1},
         {1,3,2,0,0,0,0},
-        {2,2,2,0,0,0,0},
-        {3,0,2,0,0,0,0},
+        {2,2,3,0,0,0,0},
+        {3,0,4,0,0,0,0},
         {1,1,1,0,0,0,0},
         {2,2,0,0,0,0,0},
-        {3,1,0,0,0,0,0},
+        {3,1,3,0,0,0,0},
         {1,4,2,0,0,0,0},
         {4,2,0,0,0,0,0},
-        {2,1,0,0,0,0,0},
+        {2,1,4,0,0,0,0},
         {0,3,0,0,0,0,0},
-        {3,0,1,0,0,0,0},
+        {3,0,3,0,0,0,0},
         {1,2,1,0,0,0,0},
         {2,1,1,0,0,0,0}
         
@@ -70,53 +70,30 @@ int main(){
     while(flag!=0){
         ROOM room[5]={};
         COMPULSORY compulsoryData={};
+        /*
+        この部分はいらない気がする
         room[0].size = 0;
         room[1].size = 0;
         room[2].size = 1;
         room[3].size = 1;
         room[4].size = 2;
+        */
         TEACHER teacher[5]={};
         CLASS class[5]={};
         for(i=0;i<NUMOFSUBJ;i++){
-            if(subject[i].size==2){
-                j = rand()%DAY;
-                k = rand()%PERIOD;
-                per = subject[i].teacher;
-                cla = subject[i].class;
-                room[4].date[j][k]++;
-                teacher[per].date[j][k]++;
-                class[cla].date[j][k]++;
-                subject[i].day = j;
-                subject[i].period = k;
-                subject[i].room = 4;
-                
-                
-            }else if(subject[i].size==1){
-                j = rand()%DAY;
-                k = rand()%PERIOD;
-                l = rand()%2+2;
-                per = subject[i].teacher;
-                cla = subject[i].class;
-                room[l].date[j][k]++;
-                teacher[per].date[j][k]++;
-                class[cla].date[j][k]++;
-                subject[i].day = j;
-                subject[i].period = k;
-                subject[i].room = l;
-                
-            }else{
-                j = rand()%DAY;
-                k = rand()%PERIOD;
-                l = rand()%2;
-                per = subject[i].teacher;
-                cla = subject[i].class;
-                room[l].date[j][k]++;
-                teacher[per].date[j][k]++;
-                class[cla].date[j][k]++;
-                subject[i].day = j;
-                subject[i].period = k;
-                subject[i].room = l;
-                
+            for(l=0;l<5;l++){
+                if(subject[i].size==l){
+                    j = rand()%DAY;
+                    k = rand()%PERIOD;
+                    per = subject[i].teacher;
+                    cla = subject[i].class;
+                    room[l].date[j][k]++;
+                    teacher[per].date[j][k]++;
+                    class[cla].date[j][k]++;
+                    subject[i].day = j;
+                    subject[i].period = k;
+                    subject[i].room = l;
+                }
             }
         }
         
@@ -138,33 +115,28 @@ int main(){
     printf("%d\n",count);
 }
 
-int MidCheck(int newDay,int newPeriod,int newRoom,SUBJECT targetSubject,ROOM room[],TEACHER teacher[],CLASS class[]){
-    /*
+int MidCheck(int newDay,int newPeriod,int newRoom,SUBJECT targetSubject,ROOM room[],TEACHER teacher[],CLASS class[], COMPULSORY compulsoryData){
     int oldCount = 0;
     int newCount = 0;
     int i,j,k,oldDay,oldPeriod;
-    int compulsoryData[DAY][PERIOD]={};
     oldDay = targetSubject.day;
     oldPeriod = targetSubject.period;
-    if(room[targetSubject.room].date[targetSubject.day][targetSubject.period]>=1){
-        oldCount += room[targetSubject.room].date[targetSubject.day][targetSubject.period]-1;
+    if(room[targetSubject.room].date[oldDay][oldPeriod]>=2){
+        oldCount += room[targetSubject.room].date[oldDay][oldPeriod]-1;
     }
-    if(teacher[targetSubject.teacher].date[targetSubject.day][targetSubject.period]>=1){
-        oldCount += teacher[targetSubject.teacher].date[targetSubject.day][targetSubject.period]-1;
+    if(teacher[targetSubject.teacher].date[oldDay][oldPeriod]>=2){
+        oldCount += teacher[targetSubject.teacher].date[oldDay][oldPeriod]-1;
     }
-    if(class[targetSubject.class].date[targetSubject.day][targetSubject.period]>=1){
-        oldCount += class[targetSubject.class].date[targetSubject.day][targetSubject.period]-1;
+    if(class[targetSubject.class].date[oldDay][oldPeriod]>=2){
+        oldCount += class[targetSubject.class].date[oldDay][oldPeriod]-1;
+    }
+    if(compulsoryData.date[oldDay][oldPeriod]>=2){
+        oldCount += compulsoryData.date[oldDay][oldPeriod]-1;
     }
     
-    //newRoomの方は移動したあとの評価なので1を引かない
-    if(room[newRoom].date[targetSubject.day][targetSubject.period]>=1){
-        oldCount += room[newRoom].date[targetSubject.day][targetSubject.period]-1;
-    }
-    if(teacher[targetSubject.teacher].date[targetSubject])
-    newCount += teacher[targetSubject.teacher].date[newDay][newPeriod];
-    newCount += class[targetSubject.class].date[newDay][newPeriod];
     
-    */
+    
+    
     
     
     
@@ -178,13 +150,13 @@ int FinalCheck(ROOM room[],TEACHER teacher[],CLASS class[],SUBJECT subject[],COM
     for(i=0;i<5;i++){
         for(j=0;j<DAY;j++){
             for(k=0;k<PERIOD;k++){
-                if(room[i].date[j][k]>=1){
+                if(room[i].date[j][k]>=2){
                     count += room[i].date[j][k]-1;
                 }
-                if(teacher[i].date[j][k]>=1){
+                if(teacher[i].date[j][k]>=2){
                     count += teacher[i].date[j][k]-1;
                 }
-                if(class[i].date[j][k]>=1){
+                if(class[i].date[j][k]>=2){
                     count += class[i].date[j][k]-1;
                 }
                 
@@ -193,7 +165,7 @@ int FinalCheck(ROOM room[],TEACHER teacher[],CLASS class[],SUBJECT subject[],COM
     }
     for(j=0;j<DAY;j++){
         for(k=0;k<PERIOD;k++){
-            if(compulsoryData.date[j][k]>=1){
+            if(compulsoryData.date[j][k]>=2){
                 count += compulsoryData.date[j][k]-1;
             }
         }
